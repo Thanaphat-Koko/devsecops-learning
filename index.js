234eb58625ee19d5f748592d2089435d89706ba2
Thanaphat Peth. <140236685+Thanaphat-Koko@users.noreply.github.com>
2026-02-20 14:08:51 +0700

    Update phone number in response message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! 2026 Koko 5555555555\n');
+  res.end('Hello World! 2026 Koko 9999999999\n');
 });
 
 server.listen(port, hostname, () => {
