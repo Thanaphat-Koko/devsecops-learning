5874fbd8d26e5dcf9a911a51d367c2c27458d290
Thanaphat-Koko <kokoy103@gmail.com>
2026-02-25 21:57:36 +0700

    Test pipeline for Lab3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! 2026 Koko GKE\n');
+  res.end('Hello World! 2026 Koko 555555\n');
 });
 
 server.listen(port, hostname, () => {
